a14e1e0d5f8e8357a512afb8e2df2178ebccfb0e
VeetiSo <v.sorakivi@gmail.com>
2024-02-29 15:41:38 +0200

    register and login

--- src/views/Login.tsx
+++ src/views/Login.tsx
@@ -1,11 +1,26 @@
+import {Button} from '@rneui/base';
+import {useState} from 'react';
 import LoginForm from '../components/LoginForm';
 import RegisterForm from '../components/RegisterForm';
 
 const Login = () => {
+  const [toggleRegister, setToggleRegister] = useState(false);
+  const handleToggle = () => {
+    setToggleRegister(!toggleRegister);
+  };
   return (
     <>
-      <LoginForm />
-      <RegisterForm />
+      {!toggleRegister ? (
+        <LoginForm />
+      ) : (
+        <RegisterForm handleToggle={handleToggle} />
+      )}
+      <Button
+        title={
+          !toggleRegister ? 'No account yet? Register here!' : 'Back to login.'
+        }
+        onPress={handleToggle}
+      />
     </>
   );
 };
--- src/views/Profile.tsx
+++ src/views/Profile.tsx
@@ -1,9 +1,13 @@
+import {Button} from '@rneui/base';
 import {Text, View} from 'react-native';
+import {useUserContext} from '../hooks/contextHooks';
 
 const Profile = () => {
+  const {handleLogout} = useUserContext();
   return (
     <View>
       <Text>Profile</Text>
+      <Button title="Logout" onPress={handleLogout} />
     </View>
   );
 };
