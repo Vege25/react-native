2dd11fcecb935abaef0203160ac9670638187499
VeetiSo <v.sorakivi@gmail.com>
2024-03-02 19:55:56 +0200

    update

--- App.tsx
+++ App.tsx
@@ -1,17 +1,16 @@
 import {StatusBar} from 'expo-status-bar';
-import {SafeAreaProvider} from 'react-native-safe-area-context';
 import Navigator from './src/navigators/Navigator';
+import {UpdateProvider} from './src/contexts/UpdateContext';
 import {UserProvider} from './src/contexts/userContext';
 
 const App = () => {
-  console.log('moro');
   return (
-    <SafeAreaProvider>
-      <UserProvider>
+    <UserProvider>
+      <UpdateProvider>
         <Navigator />
         <StatusBar style="auto" />
-      </UserProvider>
-    </SafeAreaProvider>
+      </UpdateProvider>
+    </UserProvider>
   );
 };
 
